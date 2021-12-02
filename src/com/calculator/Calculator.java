d80c5b4809e99f221d7f5b6da4936ed2452efaf9
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-12-02 09:12:19 -0700

    Calculator1 Complete

--- src/com/calculator/Calculator.java
+++ src/com/calculator/Calculator.java
@@ -2,12 +2,76 @@ package com.calculator;
 
 public class Calculator implements java.io.Serializable{
 
+		private double operandOne;
+		private double operandTwo;
+		private String operation;
+		private double results;
+		
+		
+
+//	Constructors	
 	public Calculator() {
+	}
+	
+	public Calculator(double value1, String operation, double value2) {
+		setOperandOne(value1);
+		setOperation(operation);
+		setOperandTwo(value2);
+		performOperation(operandOne, operation, operandTwo);
 	}
 
 	
+//	METHODS
+	public void performOperation(double operandOne, String operation, double operandTwo) {
+		if( operation == "+") {
+			setResults(operandOne + operandTwo);
+		}
+		else {
+			setResults(operandOne - operandTwo);
+		}
+		System.out.println(getResults());
+	}
+	
+//	GETTERS AND SETTERS
+	
+	public double getResults() {
+		return results;
+	}
 	
 	
+	public void setResults(double results) {
+		this.results = results;
+	}
+
+
+	public double getOperandOne() {
+		return operandOne;
+	}
+
+
+	public void setOperandOne(double operandOne) {
+		this.operandOne = operandOne;
+	}
+
+
+	public double getOperandTwo() {
+		return operandTwo;
+	}
+
+
+	public void setOperandTwo(double operandTwo) {
+		this.operandTwo = operandTwo;
+	}
+
+
+	public String getOperation() {
+		return operation;
+	}
+
+
+	public void setOperation(String operation) {
+		this.operation = operation;
+	}
 	
 	
 	
--- src/com/calculator/CalculatorTest.java
+++ src/com/calculator/CalculatorTest.java
@@ -3,8 +3,12 @@ package com.calculator;
 public class CalculatorTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		Calculator operation1 = new Calculator(10.5, "+", 5.2);
+		
+		
+		
 	}
 
 }
